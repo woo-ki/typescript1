8a2bce6f27e5ee71ab5d507ca417b6a75f413408
김영욱 <ziziza93@naver.com>
2021-11-20 20:13:40 +0900

    타입스크립트 기본 타입들(pro tips)
    ㄴ 타입을 지정하지 않으면 초기 선언될 때 타입이 자동으로 부여됨(거의 다)

---
+++ index.js
@@ -0,0 +1,12 @@
+var myName = 'wooki';
+var myAge = 29;
+var myBirthplace = '인천';
+var favorite = {
+    singer: '박효신',
+    music: '사랑한 후에'
+};
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,14 @@
+let myName: string = 'wooki';
+let myAge: number = 29;
+let myBirthplace: string = '인천';
+
+let favorite: {singer: string, music: string} = {
+    singer: '박효신',
+    music: '사랑한 후에'
+}
+
+let project: {member: string[], days: number, started: boolean} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
