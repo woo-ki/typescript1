1d62edfd085e3299e0e1acec98122c0ddfb8953a
김영욱 <ziziza93@naver.com>
2021-11-21 22:11:08 +0900

    cosnt의 상위호환 literal type

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-var animal = 13413;
-console.log(animal);
-var animal2 = {
-    name: '하마',
-    age: 3
+// literal type 원하는 문자열, 숫자같은 특정 값만 입력 가능하게 만들어주는 방법
+var introduce;
+introduce = 'wooki';
+var fn = function (x) {
+    console.log(x);
+    return 1;
 };
-console.log(animal2);
-var friend = {
-    name: '망할놈'
+fn('hello');
+var rockPaperScissors = function (x) {
+    var arr = ['가위', '바위', '보'];
+    var you = arr.indexOf(x);
+    var com = Math.floor(Math.random() * 3);
+    console.log("You: ".concat(x));
+    console.log("Com: ".concat(arr[com]));
+    switch (you - com) {
+        case 0:
+            console.log('비겼습니다!');
+            break;
+        case 1:
+        case -2:
+            console.log('이겼습니다!👍');
+            break;
+        case -1:
+        case 2:
+            console.log('졌습니다...😢');
+            break;
+        default:
+            break;
+    }
 };
-console.log(friend);
-var person = 13;
-console.log(person);
-var position = {
-    x: 134,
-    y: 23
+rockPaperScissors('보');
+// 리터럴 타입의 문제 data.name 은 'kim'이라는 타입이 아니기 때문에 문제발생
+// data를 선언할 때 타입 부여하기
+// 또는 {name: 'kim'}뒤에  as const 붙여주기
+// 오브젝트 키값의 타입을 밸류로 고정시켜주는 문법
+// + readonly 속성을 강제로 부여하는 문법
+var data = {
+    name: 'kim'
 };
-console.log(position);
-var q1 = {
-    q1_1: 1,
-    q1_2: 2,
-    q1_10: 10
+console.log(data);
+var myFn = function (x) {
+    console.log(x);
 };
-console.log(q1);
-var q2 = {
-    size: 10,
-    position: [1, 3, 5]
-};
-console.log(q2);
-var q3 = { name: 'kim', phone: 123, email: 'abc@naver.com' };
-console.log(q3);
-var q4 = {
-    name: 'kim',
-    phone: 13111,
-    email: '134@242.44',
-    isAdult: true
-};
-console.log(q4);
+myFn('kim');
+// myFn(data.name);
--- index.ts
+++ index.ts
@@ -1,95 +1,47 @@
-// 타입이 너무 길 경우 커스텀 타입(타입변수)을 활용하자(type alias)
-// 타입 변수는 대문자로 시작하는게 관습
-// 뒤에 type 을 붙여주는게 관습
-type AnimalType = string | number | undefined;
-let animal: AnimalType = 13413
-console.log(animal);
+// literal type 원하는 문자열, 숫자같은 특정 값만 입력 가능하게 만들어주는 방법
+let introduce: 'wooki' | 29;
+introduce = 'wooki';
 
-type AnimalType2 = {
-    name: string,
-    age: number
+const fn = (x: 'hello'):(1 | 0) => {
+    console.log(x);
+    return 1;
 }
-let animal2: AnimalType2 = {
-    name: '하마',
-    age: 3
-}
-console.log(animal2)
+fn('hello');
 
-// object와 array의 값을 수정할 수 없도록 막아주는 readonly 속성
-type FriendType = {
-    readonly name: string
+// 가위바위보 머신 만들기
+type RockPaperScissors = '가위' | '바위' | '보';
+const rockPaperScissors = (x: RockPaperScissors): void => {
+    const arr: string[] = ['가위', '바위', '보']
+    const you: number = arr.indexOf(x);
+    const com: number = Math.floor(Math.random() * 3);
+    console.log(`You: ${x}`);
+    console.log(`Com: ${arr[com]}`);
+    switch (you - com) {
+        case 0:
+            console.log('비겼습니다!'); break;
+        case 1:
+        case -2:
+            console.log('이겼습니다!👍'); break;
+        case -1:
+        case 2:
+            console.log('졌습니다...😢'); break;
+        default:
+            break;
+    }
 }
-const friend: FriendType = {
-    name: '망할놈'
-}
-console.log(friend);
+rockPaperScissors('보');
 
-// 타입은 여러개를 합쳐서 만들 수 있음
-// 단, 재정의는 불가능함
-// 여러개의 타입을 | 로 or 조건으로 묶어주거나 object 같은 타입은 & 기호로 extend 해주기
-type NameType = string;
-type AgeType = number;
-type PersonType = NameType | AgeType;
-let person: PersonType = 13;
-console.log(person);
-
-type PositionXType = {x: number};
-type PositionYType = {y: number};
-type NewType = PositionXType & PositionYType;   // object 타입들의 extend
-let position: NewType = {
-    x: 134,
-    y: 23
+// 리터럴 타입의 문제 data.name 은 'kim'이라는 타입이 아니기 때문에 문제발생
+// data를 선언할 때 타입 부여하기
+// 또는 {name: 'kim'}뒤에  as const 붙여주기
+// 오브젝트 키값의 타입을 밸류로 고정시켜주는 문법
+// + readonly 속성을 강제로 부여하는 문법
+let data = {
+    name: 'kim'
 }
-console.log(position);
-
-// (숙제1) object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면 어떻게 될까요?
-type Q1_1 = {q1_1: number, q1_10: number};
-type Q1_2 = {q1_2: number, q1_10: number};
-type Q1_3 = Q1_1 & Q1_2;
-let q1: Q1_3 = {
-    q1_1: 1,
-    q1_2: 2,
-    q1_10: 10
-};
-console.log(q1);
-
-// (숙제2) 다음 조건을 만족하는 타입을 만들어봅시다.
-// 1. 이 타입은 object 자료형이어야합니다.
-// 2. 이 타입은 color 라는 속성을 가질 수도 있으며 항상 문자가 들어와야합니다.
-// 3. 이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
-// 4. 이 타입은 position 이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야합니다.
-type Q2 = {
-    color?: string,
-    size: number,
-    readonly position: number[]
+console.log(data);
+const myFn = (x: 'kim') => {
+    console.log(x);
 }
-const q2: Q2 = {
-    size: 10,
-    position: [1, 3, 5]
-}
-console.log(q2);
-
-// (숙제3) 다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오.
-// 1. 대충 이렇게 생긴 object 자료를 다룰 일이 많습니다. { name : 'kim', phone : 123, email : 'abc@naver.com' }
-// 2. object 안에 있는 이름, 전화번호, 이메일 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
-// 3. 각 속성이 어떤 타입일지는 자유롭게 정하십시오.
-type Q3 = {
-    name: string,
-    phone: number,
-    email: string
-}
-const q3: Q3 = { name : 'kim', phone : 123, email : 'abc@naver.com' }
-console.log(q3);
-
-// (숙제4). 다음을 만족하는 type alias를 만들어보십시오.
-// 1. 숙제2와 똑같은데 이번엔 이름, 전화번호, 이메일, 미성년자여부 속성을 옳은 타입인지 검사하는 type alias를 만들어봅시다.
-// 2. 미성년자 여부 속성은 true/false만 들어올 수 있습니다.
-// 3. 멋있게 숙제2에서 만들어둔  type alias를 재활용해봅시다.
-type Q4 = Q3 & {isAdult: boolean}
-const q4: Q4 = {
-    name: 'kim',
-    phone: 13111,
-    email: '134@242.44',
-    isAdult: true
-}
-console.log(q4);
+myFn('kim');
+// myFn(data.name);
