10d3a9173c6c32813d11005cd40d00c7dcc4dd2a
김영욱 <ziziza93@naver.com>
2021-11-25 00:33:14 +0900

    타입스크립트로 엘리먼트를 수정할 때는 내로잉을 잘 해야한다.

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-var func = function (x) {
-    return Number(x);
+// 타입스크립트에서 엘리먼트 조작시 주의사항
+// title 은 null 이거나 엘리먼트 이거나(유니온타입)
+// 내로잉 해줘야함.
+var title = document.getElementById('title');
+title instanceof Element ? title.textContent = '하이요' : null;
+var anchor = document.querySelector('#link');
+anchor instanceof HTMLAnchorElement ? anchor.href = 'https://kakao.com' : null;
+// 내로잉 하는 다른 방법(2020년 부터 가능, 신문법) / 옵셔널 체이닝
+// element?.xxx => element 가 xxx 를 사용할 수 있으면 진행해주고 아니면 undefined 반환
+// 이벤트의 타입은 Event
+// bindTest 자리에 바로 introduce 펑션을 넣어도 이벤트는 알아서 파라미터로 넘어감.
+// 언바인딩을 하기 위해서는 바인딩 하는 이벤트에 이름지정이 필수
+var introduce = function (e, x) {
+    console.log(e);
+    console.log(x);
+    console.log(e.target);
 };
-func('3');
-var member = {
-    name: 'kim',
-    plusOne: function (x) {
-        return x + 1;
-    },
-    changeName: function () {
-        this.name = 'wooki';
-    }
-};
-console.log(member.plusOne(2));
-console.log(member);
-member.changeName();
-console.log(member);
-var cutZero = function (x) {
-    var target = x.charAt(0);
-    if (target === '0') {
-        return x.substring(1);
-    }
-    else {
-        return x;
-    }
-};
-console.log(cutZero('0aax0'));
-console.log(cutZero('adfsa'));
-var removeDash = function (x) {
-    var temp = x.replace(/-/g, '');
-    var result = Number(temp);
-    if (isNaN(result)) {
-        return -1;
-    }
-    else {
-        return result;
-    }
-};
-console.log(removeDash('1as-'));
-console.log(removeDash('1-1-1--'));
-var finalFunc = function (x, func1, func2) {
-    var temp = func1(x);
-    var result = func2(temp);
-    console.log(result);
-};
-finalFunc('010-1111-2222', cutZero, removeDash);
+var btn = document.querySelector('#button');
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', function bindTest(e) {
+    introduce(e, '안뇽~');
+    btn === null || btn === void 0 ? void 0 : btn.removeEventListener('click', bindTest);
+});
+// (숙제1) 버튼을 누르면 이미지를 바꿔봅시다.
+// <img id="image" src="test.jpg">
+// html 안에 test.jpg 를 보여주고 있는 이미지 태그가 있다고 칩시다.
+// 이미지를 new.jpg 라는 이미지로 바꾸고 싶으면 자바스크립트 코드를 어떻게 짜야할까요?
+// 성공여부는 크롬 개발자도구 켜면 src 속성이 잘 바뀌었는지 확인가능하겠죠?
+var img = document.querySelector('#image');
+img instanceof HTMLImageElement ? img.src = 'change.jpg' : null;
+// (숙제2) 바꾸고 싶은 html 요소가 많습니다.
+// <a class="naver" href="naver.com">링크</a>
+// <a class="naver" href="naver.com">링크</a>
+// <a class="naver" href="naver.com">링크</a>
+// 3개의 링크가 있는데 이 요소들의 href 속성을 전부 https://kakao.com으로 바꾸고 싶은 겁니다.
+// 자바스크립트 코드를 어떻게 짜야할까요?
+var anchorList = document.querySelectorAll('.naver');
+anchorList.forEach(function (t) {
+    t instanceof HTMLAnchorElement ? t.href = 'https://kakao.com' : null;
+});
--- index.ts
+++ index.ts
@@ -1,76 +1,43 @@
-// 메소드에 타입을 커스텀해서 만드는 방법
-type FuncType = (x: string) => number;
+// 타입스크립트에서 엘리먼트 조작시 주의사항
+// title 은 null 이거나 엘리먼트 이거나(유니온타입)
+// 내로잉 해줘야함.
+const title = document.getElementById('title');
+title instanceof Element ? title.textContent = '하이요' : null;
 
-const func: FuncType = (x) => {
-    return Number(x);
-}
-func('3');
+const anchor = document.querySelector('#link');
+anchor instanceof HTMLAnchorElement ? anchor.href = 'https://kakao.com' : null;
 
-// 오브젝트 내부의 메소드에 메소드타입 지정하는 방법
-type FuncType2 = (x: number) => number;
-type FuncType3 = () => void;
-type MemberType = {
-    name: string,
-    plusOne: FuncType2,
-    changeName: FuncType3
+// 내로잉 하는 다른 방법(2020년 부터 가능, 신문법) / 옵셔널 체이닝
+// element?.xxx => element 가 xxx 를 사용할 수 있으면 진행해주고 아니면 undefined 반환
+// 이벤트의 타입은 Event
+// bindTest 자리에 바로 introduce 펑션을 넣어도 이벤트는 알아서 파라미터로 넘어감.
+// 언바인딩을 하기 위해서는 바인딩 하는 이벤트에 이름지정이 필수
+const introduce = (e: Event, x: string) => {
+    console.log(e);
+    console.log(x);
+    console.log(e.target);
 }
-const member:MemberType = {
-    name: 'kim',
-    plusOne: (x) => {
-        return x + 1
-    },
-    changeName() {
-        this.name = 'wooki';
-    }
-}
-console.log(member.plusOne(2));
-console.log(member);
-member.changeName();
-console.log(member);
+const btn = document.querySelector('#button');
+btn?.addEventListener('click', function bindTest(e: Event) {
+    introduce(e, '안뇽~');
+    btn?.removeEventListener('click', bindTest);
+});
 
-// (숙제1) 다음 함수2개를 만들어보고 타입까지 정의해보십시오.
-// - cutZero()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 맨 앞에 '0' 문자가 있으면 제거하고 문자 type으로 return 해줍니다.
-// - removeDash()라는 함수를 만듭시다. 이 함수는 문자를 하나 입력하면 대시기호 '-' 가 있으면 전부 제거해주고 그걸 숫자 type으로 return 해줍니다.
-// - 함수에 타입지정시 type alias를 꼭 써보도록 합시다.
-// 물론 문자제거 하는 방법을 모른다면 구글검색이 필요합니다.
-type CutZeroType = (x: string) => string
-const cutZero: CutZeroType = (x) => {
-    const target = x.charAt(0);
-    if(target === '0') {
-        return x.substring(1);
-    } else {
-        return x;
-    }
-}
-console.log(cutZero('0aax0'));
-console.log(cutZero('adfsa'));
+// (숙제1) 버튼을 누르면 이미지를 바꿔봅시다.
+// <img id="image" src="test.jpg">
+// html 안에 test.jpg 를 보여주고 있는 이미지 태그가 있다고 칩시다.
+// 이미지를 new.jpg 라는 이미지로 바꾸고 싶으면 자바스크립트 코드를 어떻게 짜야할까요?
+// 성공여부는 크롬 개발자도구 켜면 src 속성이 잘 바뀌었는지 확인가능하겠죠?
+const img = document.querySelector('#image');
+img instanceof HTMLImageElement ? img.src = 'change.jpg' : null;
 
-type RemoveDashType = (x: string) => number
-const removeDash: RemoveDashType = (x) => {
-    const temp = x.replace(/-/g, '');
-    const result = Number(temp);
-    if(isNaN(result)) {
-        return -1;
-    } else {
-        return result;
-    }
-}
-console.log(removeDash('1as-'));
-console.log(removeDash('1-1-1--'));
-
-// (숙제3) 함수에 함수를 집어넣고 싶습니다.
-// 숙제2에서 만든 함수들을 파라미터로 넣을 수 있는 함수를 제작하고 싶은 것입니다.
-// 이 함수는 파라미터 3개가 들어가는데 첫째는 문자, 둘째는 함수, 셋째는 함수를 집어넣을 수 있습니다. 이 함수를 실행하면
-// 1. 첫째 파라미터를 둘째 파라미터 (함수)에 파라미터로 집어넣어줍니다.
-// 2. 둘째 파라미터 (함수)에서 return된 결과를 셋째 파라미터(함수)에 집어넣어줍니다.
-// 3. 셋째 파라미터 (함수)에서 return된 결과를 콘솔창에 출력해줍니다.
-// 이 함수는 어떻게 만들면 될까요?
-// 둘째 파라미터엔 cutZero, 셋째 파라미터엔 removeDash 라는 함수들만 입력할 수 있게 파라미터의 타입도 지정해봅시다.
-// ex ) 만들함수('010-1111-2222', cutZero, removeDash)
-type FinalFuncType = (x: string, func1: CutZeroType, func2: RemoveDashType) => void;
-const finalFunc: FinalFuncType = (x, func1, func2) => {
-    const temp = func1(x);
-    const result = func2(temp);
-    console.log(result);
-}
-finalFunc('010-1111-2222', cutZero, removeDash);
+// (숙제2) 바꾸고 싶은 html 요소가 많습니다.
+// <a class="naver" href="naver.com">링크</a>
+// <a class="naver" href="naver.com">링크</a>
+// <a class="naver" href="naver.com">링크</a>
+// 3개의 링크가 있는데 이 요소들의 href 속성을 전부 https://kakao.com으로 바꾸고 싶은 겁니다.
+// 자바스크립트 코드를 어떻게 짜야할까요?
+const anchorList = document.querySelectorAll('.naver');
+anchorList.forEach((t) => {
+    t instanceof HTMLAnchorElement ? t.href = 'https://kakao.com' : null;
+});
