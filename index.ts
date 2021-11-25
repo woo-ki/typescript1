b48ed94e1c00b1c861f9188cb93cdd6183922789
김영욱 <ziziza93@naver.com>
2021-11-26 00:59:23 +0900

    클래스에 타입 지정하는 방법

--- index.ts
+++ index.ts
@@ -1,43 +1,75 @@
-// 타입스크립트에서 엘리먼트 조작시 주의사항
-// title 은 null 이거나 엘리먼트 이거나(유니온타입)
-// 내로잉 해줘야함.
-const title = document.getElementById('title');
-title instanceof Element ? title.textContent = '하이요' : null;
+// 클래스 생성시 타입지정하는 방법
+class Person {
+    name: string;
 
-const anchor = document.querySelector('#link');
-anchor instanceof HTMLAnchorElement ? anchor.href = 'https://kakao.com' : null;
+    constructor(name: string) {
+        this.name = name;
+    }
 
-// 내로잉 하는 다른 방법(2020년 부터 가능, 신문법) / 옵셔널 체이닝
-// element?.xxx => element 가 xxx 를 사용할 수 있으면 진행해주고 아니면 undefined 반환
-// 이벤트의 타입은 Event
-// bindTest 자리에 바로 introduce 펑션을 넣어도 이벤트는 알아서 파라미터로 넘어감.
-// 언바인딩을 하기 위해서는 바인딩 하는 이벤트에 이름지정이 필수
-const introduce = (e: Event, x: string) => {
-    console.log(e);
-    console.log(x);
-    console.log(e.target);
+    test = (): void => {
+        console.log(`hi~ i'm ${this.name}`);
+    }
 }
-const btn = document.querySelector('#button');
-btn?.addEventListener('click', function bindTest(e: Event) {
-    introduce(e, '안뇽~');
-    btn?.removeEventListener('click', bindTest);
-});
 
-// (숙제1) 버튼을 누르면 이미지를 바꿔봅시다.
-// <img id="image" src="test.jpg">
-// html 안에 test.jpg 를 보여주고 있는 이미지 태그가 있다고 칩시다.
-// 이미지를 new.jpg 라는 이미지로 바꾸고 싶으면 자바스크립트 코드를 어떻게 짜야할까요?
-// 성공여부는 크롬 개발자도구 켜면 src 속성이 잘 바뀌었는지 확인가능하겠죠?
-const img = document.querySelector('#image');
-img instanceof HTMLImageElement ? img.src = 'change.jpg' : null;
+const person1 = new Person('kim');
+const person2 = new Person('park');
+console.log(person1);
+console.log(person2);
+person1.test();
+person2.test();
 
-// (숙제2) 바꾸고 싶은 html 요소가 많습니다.
-// <a class="naver" href="naver.com">링크</a>
-// <a class="naver" href="naver.com">링크</a>
-// <a class="naver" href="naver.com">링크</a>
-// 3개의 링크가 있는데 이 요소들의 href 속성을 전부 https://kakao.com으로 바꾸고 싶은 겁니다.
-// 자바스크립트 코드를 어떻게 짜야할까요?
-const anchorList = document.querySelectorAll('.naver');
-anchorList.forEach((t) => {
-    t instanceof HTMLAnchorElement ? t.href = 'https://kakao.com' : null;
-});
+// (숙제1) Car 클래스를 만들고 싶습니다.
+// 1. 대충 { model : '소나타', price : 3000 } 이렇게 생긴 object를 복사해주는 class를 만들어보십시오.
+// 2. 그리고 복사된 object 자료들은 .tax() 라는 함수를 사용가능한데 현재 object에 저장된 price의 10분의1을 출력해주어야합니다.
+// 3. model과 price 속성의 타입지정도 알아서 잘 해보십시오. tax() 함수의 return 타입도요.
+//
+// (동작 예시)
+// let car1 = new Car('소나타', 3000)
+// console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+// console.log(car1.tax()) //콘솔창 출력결과는 300
+class Car {
+    model: string;
+    price: number;
+    constructor(model: string, price: number) {
+        this.model = model;
+        this.price = price;
+    }
+
+    tax = (): number => {
+        return this.price / 10;
+    }
+}
+const car1 = new Car('소나타', 3000);
+console.log(car1);
+console.log(car1.tax());
+
+// (숙제2) class인데 파라미터가 잔뜩 들어가는 class Word를 만들어봅시다.
+// 1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
+// 2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
+// 3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
+// 4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 마셈
+//
+// (동작 예시)
+// let obj = new Word('kim', 3, 5, 'park');
+// console.log(obj.num) //[3,5]
+// console.log(obj.str) //['kim', 'park']
+class Word {
+    num: number[];
+    str: string[];
+    constructor(...args: (string | number)[]) {
+        const tempNum: number[] = [];
+        const tempStr: string[] = [];
+        args.forEach((t: string | number): void => {
+            if(typeof t === 'number') {
+                tempNum.push(t);
+            } else {
+                tempStr.push(t);
+            }
+        });
+        this.num = tempNum;
+        this.str = tempStr;
+    }
+}
+
+const obj = new Word('kim', 3, 5, 'park', '후후');
+console.log(obj);
