4c5551113d3398c75cc26b0a2c1cdce9930bed33
김영욱 <ziziza93@naver.com>
2021-11-26 01:23:18 +0900

    오브젝트에는 type 대신 interface

--- index.js
+++ index.js
@@ -1,73 +1,27 @@
-// 클래스 생성시 타입지정하는 방법
-var Person = /** @class */ (function () {
-    function Person(name) {
-        var _this = this;
-        this.test = function () {
-            console.log("hi~ i'm ".concat(_this.name));
-        };
-        this.name = name;
+var rect = { color: 'red', width: 100 };
+console.log(rect);
+var square = {
+    color: 'blue',
+    width: 50
+};
+console.log(square);
+var student = { name: 'kim' };
+var teacher = { name: 'kim', age: 37, score: 100 };
+console.log(student);
+console.log(teacher);
+var product = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
+console.log(product);
+var cart = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800 }];
+console.log(cart);
+var cart2 = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800, card: true }];
+console.log(cart2);
+var methods = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
     }
-    return Person;
-}());
-var person1 = new Person('kim');
-var person2 = new Person('park');
-console.log(person1);
-console.log(person2);
-person1.test();
-person2.test();
-// (숙제1) Car 클래스를 만들고 싶습니다.
-// 1. 대충 { model : '소나타', price : 3000 } 이렇게 생긴 object를 복사해주는 class를 만들어보십시오.
-// 2. 그리고 복사된 object 자료들은 .tax() 라는 함수를 사용가능한데 현재 object에 저장된 price의 10분의1을 출력해주어야합니다.
-// 3. model과 price 속성의 타입지정도 알아서 잘 해보십시오. tax() 함수의 return 타입도요.
-//
-// (동작 예시)
-// let car1 = new Car('소나타', 3000)
-// console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
-// console.log(car1.tax()) //콘솔창 출력결과는 300
-var Car = /** @class */ (function () {
-    function Car(model, price) {
-        var _this = this;
-        this.tax = function () {
-            return _this.price / 10;
-        };
-        this.model = model;
-        this.price = price;
-    }
-    return Car;
-}());
-var car1 = new Car('소나타', 3000);
-console.log(car1);
-console.log(car1.tax());
-// (숙제2) class인데 파라미터가 잔뜩 들어가는 class Word를 만들어봅시다.
-// 1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
-// 2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
-// 3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
-// 4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 마셈
-//
-// (동작 예시)
-// let obj = new Word('kim', 3, 5, 'park');
-// console.log(obj.num) //[3,5]
-// console.log(obj.str) //['kim', 'park']
-var Word = /** @class */ (function () {
-    function Word() {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        var tempNum = [];
-        var tempStr = [];
-        args.forEach(function (t) {
-            if (typeof t === 'number') {
-                tempNum.push(t);
-            }
-            else {
-                tempStr.push(t);
-            }
-        });
-        this.num = tempNum;
-        this.str = tempStr;
-    }
-    return Word;
-}());
-var obj = new Word('kim', 3, 5, 'park', '후후');
-console.log(obj);
+};
+console.log(methods.plus(5, 3));
+console.log(methods.minus(5, 3));
--- index.ts
+++ index.ts
@@ -1,75 +1,93 @@
-// 클래스 생성시 타입지정하는 방법
-class Person {
-    name: string;
+// object의 타입 지정하는 방법 두번째
+// 1번은 type 사용
+// 2번은 interface
+type Rect = {color: string, width: number}
+const rect:Rect = {color: 'red', width: 100}
+console.log(rect);
 
-    constructor(name: string) {
-        this.name = name;
-    }
+interface Square {
+    color: string,
+    width: number
+}
+const square: Square = {
+    color: 'blue',
+    width: 50
+}
+console.log(square);
 
-    test = (): void => {
-        console.log(`hi~ i'm ${this.name}`);
-    }
+// type VS interface
+// 인터페이스의 장점 -- extends 기능을 제공함
+// type도 &를 이용해서 복사를 할 수 있기는 함(불편..) // 인터섹션 이라는 방법 intersection type
+// interface = 중복선언 가능 , type = 중복선언 불가능
+// 중복선언시 기존 항목에 추가가됨
+// interface 를 상속하거나 추가로 중복선언 할 때 이미 사용중인 속성을 기입할경우 에러발생
+interface Student {
+    name: string
+}
+interface Teacher extends Student {
+    age: number
+}
+interface Student {
+    score?: number
 }
 
-const person1 = new Person('kim');
-const person2 = new Person('park');
-console.log(person1);
-console.log(person2);
-person1.test();
-person2.test();
+const student: Student = {name: 'kim'}
+const teacher: Teacher = {name: 'kim', age: 37, score: 100}
+console.log(student);
+console.log(teacher);
 
-// (숙제1) Car 클래스를 만들고 싶습니다.
-// 1. 대충 { model : '소나타', price : 3000 } 이렇게 생긴 object를 복사해주는 class를 만들어보십시오.
-// 2. 그리고 복사된 object 자료들은 .tax() 라는 함수를 사용가능한데 현재 object에 저장된 price의 10분의1을 출력해주어야합니다.
-// 3. model과 price 속성의 타입지정도 알아서 잘 해보십시오. tax() 함수의 return 타입도요.
-//
-// (동작 예시)
-// let car1 = new Car('소나타', 3000)
-// console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
-// console.log(car1.tax()) //콘솔창 출력결과는 300
-class Car {
-    model: string;
-    price: number;
-    constructor(model: string, price: number) {
-        this.model = model;
-        this.price = price;
-    }
 
-    tax = (): number => {
-        return this.price / 10;
+// (숙제1) interface 이용해서 간단하게 타입을 만들어봅시다
+// let 상품 = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+// 이런 변수가 있는데 interface 키워드로 타입지정 이쁘게 하고 싶습니다. 어떻게 코드를 짜면 될까요?
+// 무슨 타입일지는 알아서 기입합시다.
+interface Product {
+    brand: string,
+    serialNumber: number,
+    model: string[]
+}
+const product: Product = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+console.log(product);
+
+// (숙제2) array 안에 object 여러개가 필요합니다.
+// 쇼핑몰 장바구니를 구현하려고 하는데
+// let 장바구니 = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+// 이렇게 생긴 object들이 잔뜩 들어갈 수 있는 array는 어떻게 타입을 지정해야할까요?
+// 오늘 배운 interface 문법을 써봅시다.
+interface CartItem {
+    product: string,
+    price: number
+}
+type Cart = CartItem[]
+const cart: Cart = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+console.log(cart);
+
+// (숙제3) 위에서 만든 타입을 extends 해봅시다.
+// 갑자기 서비스가 업데이트되어서 일부 상품은 card 속성이 들어가야합니다.
+// { product : '청소기', price : 7000, card : false }
+// 위에서 만든 interface를 extends 해서 이 object의 타입을 만들어보십시오.
+interface CartItem2 extends CartItem {
+    card?: boolean
+}
+const cart2: CartItem2[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800, card: true } ]
+console.log(cart2);
+
+// (숙제4) object 안에 함수를 2개 넣고 싶은데요
+// 1. 이 object 자료는 plus() 함수를 내부에 가지고 있으며 plus 함수는 파라미터 2개를 입력하면 더해서 return 해줍니다.
+// 2. 이 object 자료는 minus() 함수를 내부에 가지고 있으며 minus 함수는 파라미터 2개를 입력하면 빼서 return 해줍니다.
+// 이 object 자료를 어떻게 만들면 될까요?
+// interface를 이용해서 object에 타입지정도 해보십시오.
+interface Methods {
+    plus: (a: number, b: number) => number,
+    minus: (a: number, b: number) => number
+}
+const methods: Methods = {
+    plus: (a, b) => {
+        return a + b;
+    },
+    minus: (a, b) => {
+        return a - b;
     }
 }
-const car1 = new Car('소나타', 3000);
-console.log(car1);
-console.log(car1.tax());
-
-// (숙제2) class인데 파라미터가 잔뜩 들어가는 class Word를 만들어봅시다.
-// 1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
-// 2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
-// 3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
-// 4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 마셈
-//
-// (동작 예시)
-// let obj = new Word('kim', 3, 5, 'park');
-// console.log(obj.num) //[3,5]
-// console.log(obj.str) //['kim', 'park']
-class Word {
-    num: number[];
-    str: string[];
-    constructor(...args: (string | number)[]) {
-        const tempNum: number[] = [];
-        const tempStr: string[] = [];
-        args.forEach((t: string | number): void => {
-            if(typeof t === 'number') {
-                tempNum.push(t);
-            } else {
-                tempStr.push(t);
-            }
-        });
-        this.num = tempNum;
-        this.str = tempStr;
-    }
-}
-
-const obj = new Word('kim', 3, 5, 'park', '후후');
-console.log(obj);
+console.log(methods.plus(5, 3));
+console.log(methods.minus(5, 3));
