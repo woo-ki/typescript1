0a4983ad2494fc6a146e21eef3c4e474ac199057
김영욱 <ziziza93@naver.com>
2021-11-26 23:44:27 +0900

    파라미터를 무한정 받는 rest parameter 그리고 도움되는 spread operator, destructuring

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
-var rect = { color: 'red', width: 100 };
-console.log(rect);
-var square = {
-    color: 'blue',
-    width: 50
-};
-console.log(square);
-var student = { name: 'kim' };
-var teacher = { name: 'kim', age: 37, score: 100 };
-console.log(student);
-console.log(teacher);
-var product = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
-console.log(product);
-var cart = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800 }];
-console.log(cart);
-var cart2 = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800, card: true }];
-console.log(cart2);
-var methods = {
-    plus: function (a, b) {
-        return a + b;
-    },
-    minus: function (a, b) {
-        return a - b;
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
     }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
-console.log(methods.plus(5, 3));
-console.log(methods.minus(5, 3));
+// rest parameter 파라미터가 몇개일지 모를 때 사용
+var func = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    console.log(args);
+};
+func(1, 2, 3, 4, 56, 67, 65, 54, 4, 23, 2);
+// spread operator 배열, 오브젝트의 내용물만 추출
+var arr = [1, 2, 3];
+var arr2 = ['4', '5'];
+var arr3 = __spreadArray(__spreadArray([], arr, true), arr2, true);
+console.log(arr3);
+// destructuring 활용해서 배열과 오브젝트 안에있는 항목을 바로 변수에 할당
+var arr4 = [1, 2];
+var a = arr4[0], b = arr4[1];
+console.log(a);
+console.log(b);
+var _a = { student: true, age: 20 }, student = _a.student, age = _a.age;
+console.log(student);
+console.log(age);
+var obj = { student: true, age: 20 };
+var func2 = function (_a) {
+    var student = _a.student, age = _a.age;
+    console.log(student, age);
+};
+func2(obj);
+// (숙제1) 숫자 여러개를 입력하면 최댓값을 return 해주는 함수를 만들어봅시다.
+// 최댓값(6,3,7,2) 이렇게 쓰면 7이 return 되어야합니다.
+// (조건1) 넣을 수 있는 숫자 갯수는 제한없음, 0 이상의 정수만 가능합니다.
+// (조건2) Math.max() 사용금지 반복문이나 쓰셈
+var homework = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args.reduce(function (a, b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    });
+};
+console.log(homework(6, 30, 7, 2));
+var homework2 = function (_a) {
+    var user = _a.user, comment = _a.comment, admin = _a.admin;
+    console.log(user);
+    console.log(comment);
+    console.log(admin);
+};
+homework2({ user: 'kim', comment: [3, 5, 4], admin: false });
+// (숙제3) 이렇게 생긴 array 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
+// 함수( [40, 'wine', false] )
+// 어떻게 코드를 짜야할까요?
+// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
+// (조건2) 함수실행시 입력한 파라미터들을 전부 콘솔창에 출력해줘야합니다.
+var homework3 = function (_a) {
+    var a = _a[0], b = _a[1], c = _a[2];
+    console.log(a, b, c);
+};
+homework3([40, 'wine', false]);
--- index.ts
+++ index.ts
@@ -1,93 +1,68 @@
-// object의 타입 지정하는 방법 두번째
-// 1번은 type 사용
-// 2번은 interface
-type Rect = {color: string, width: number}
-const rect:Rect = {color: 'red', width: 100}
-console.log(rect);
+// rest parameter 파라미터가 몇개일지 모를 때 사용
+const func = (...args: number[]) => {
+    console.log(args)
+}
+func(1,2,3,4,56,67,65,54,4,23,2);
 
-interface Square {
-    color: string,
-    width: number
-}
-const square: Square = {
-    color: 'blue',
-    width: 50
-}
-console.log(square);
+// spread operator 배열, 오브젝트의 내용물만 추출
+const arr: number[] = [1, 2, 3];
+const arr2: string[] = ['4', '5'];
+const arr3: (number | string)[] = [...arr, ...arr2];
+console.log(arr3);
 
-// type VS interface
-// 인터페이스의 장점 -- extends 기능을 제공함
-// type도 &를 이용해서 복사를 할 수 있기는 함(불편..) // 인터섹션 이라는 방법 intersection type
-// interface = 중복선언 가능 , type = 중복선언 불가능
-// 중복선언시 기존 항목에 추가가됨
-// interface 를 상속하거나 추가로 중복선언 할 때 이미 사용중인 속성을 기입할경우 에러발생
-interface Student {
-    name: string
-}
-interface Teacher extends Student {
-    age: number
-}
-interface Student {
-    score?: number
-}
-
-const student: Student = {name: 'kim'}
-const teacher: Teacher = {name: 'kim', age: 37, score: 100}
+// destructuring 활용해서 배열과 오브젝트 안에있는 항목을 바로 변수에 할당
+const arr4: number[] = [1, 2];
+const [a, b] = arr4;
+console.log(a);
+console.log(b);
+const {student, age} = {student: true, age: 20}
 console.log(student);
-console.log(teacher);
+console.log(age);
 
+const obj = {student: true, age: 20}
+const func2 = ({student, age}: {student: boolean, age: number}) => {
+    console.log(student, age);
+}
+func2(obj);
 
-// (숙제1) interface 이용해서 간단하게 타입을 만들어봅시다
-// let 상품 = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
-// 이런 변수가 있는데 interface 키워드로 타입지정 이쁘게 하고 싶습니다. 어떻게 코드를 짜면 될까요?
-// 무슨 타입일지는 알아서 기입합시다.
-interface Product {
-    brand: string,
-    serialNumber: number,
-    model: string[]
+// (숙제1) 숫자 여러개를 입력하면 최댓값을 return 해주는 함수를 만들어봅시다.
+// 최댓값(6,3,7,2) 이렇게 쓰면 7이 return 되어야합니다.
+// (조건1) 넣을 수 있는 숫자 갯수는 제한없음, 0 이상의 정수만 가능합니다.
+// (조건2) Math.max() 사용금지 반복문이나 쓰셈
+const homework = (...args: number[]): number => {
+    return args.reduce((a: number, b: number): number => {
+        if(a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    });
 }
-const product: Product = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
-console.log(product);
+console.log(homework(6, 30, 7, 2));
 
-// (숙제2) array 안에 object 여러개가 필요합니다.
-// 쇼핑몰 장바구니를 구현하려고 하는데
-// let 장바구니 = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
-// 이렇게 생긴 object들이 잔뜩 들어갈 수 있는 array는 어떻게 타입을 지정해야할까요?
-// 오늘 배운 interface 문법을 써봅시다.
-interface CartItem {
-    product: string,
-    price: number
+// (숙제2) 이렇게 생긴 object 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
+// 함수( { user : 'kim', comment : [3,5,4], admin : false } )
+// 어떻게 코드를 짜야할까요?
+// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
+// (조건2) 함수실행시 입력한 파라미터의 value들 (kim, [3,5,4] 이런거)을 전부 콘솔창에 출력해줘야합니다.
+interface Homework2 {
+    user: string,
+    comment: number[],
+    admin: boolean
 }
-type Cart = CartItem[]
-const cart: Cart = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
-console.log(cart);
+const homework2 = ({user, comment, admin}: Homework2): void => {
+    console.log(user);
+    console.log(comment);
+    console.log(admin);
+}
+homework2({user : 'kim', comment : [3,5,4], admin : false});
 
-// (숙제3) 위에서 만든 타입을 extends 해봅시다.
-// 갑자기 서비스가 업데이트되어서 일부 상품은 card 속성이 들어가야합니다.
-// { product : '청소기', price : 7000, card : false }
-// 위에서 만든 interface를 extends 해서 이 object의 타입을 만들어보십시오.
-interface CartItem2 extends CartItem {
-    card?: boolean
+// (숙제3) 이렇게 생긴 array 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
+// 함수( [40, 'wine', false] )
+// 어떻게 코드를 짜야할까요?
+// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
+// (조건2) 함수실행시 입력한 파라미터들을 전부 콘솔창에 출력해줘야합니다.
+const homework3 = ([a, b, c]: (string | number | boolean)[]): void => {
+    console.log(a, b, c);
 }
-const cart2: CartItem2[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800, card: true } ]
-console.log(cart2);
-
-// (숙제4) object 안에 함수를 2개 넣고 싶은데요
-// 1. 이 object 자료는 plus() 함수를 내부에 가지고 있으며 plus 함수는 파라미터 2개를 입력하면 더해서 return 해줍니다.
-// 2. 이 object 자료는 minus() 함수를 내부에 가지고 있으며 minus 함수는 파라미터 2개를 입력하면 빼서 return 해줍니다.
-// 이 object 자료를 어떻게 만들면 될까요?
-// interface를 이용해서 object에 타입지정도 해보십시오.
-interface Methods {
-    plus: (a: number, b: number) => number,
-    minus: (a: number, b: number) => number
-}
-const methods: Methods = {
-    plus: (a, b) => {
-        return a + b;
-    },
-    minus: (a, b) => {
-        return a - b;
-    }
-}
-console.log(methods.plus(5, 3));
-console.log(methods.minus(5, 3));
+homework3([40, 'wine', false]);
