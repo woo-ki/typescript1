a9cbcb39ac03c645d6af85e35fcb7771233fe524
김영욱 <ziziza93@naver.com>
2021-11-27 00:23:18 +0900

    내로잉 추가방법 in, 강제로 리터럴타입 추가

--- index.js
+++ index.js
@@ -1,73 +1,28 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+// null & undefined 체크 팁
+var func = function (a) {
+    if (a && typeof a === 'string') {
+        // a가 null 또는 undefined 일때는 if문 실행이 안됨 false 로 인식함 바로
+        // 익숙하지 않으면 안쓰는게 나음 ㅇㅇ
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// rest parameter 파라미터가 몇개일지 모를 때 사용
-var func = function () {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
+    else {
     }
-    console.log(args);
 };
-func(1, 2, 3, 4, 56, 67, 65, 54, 4, 23, 2);
-// spread operator 배열, 오브젝트의 내용물만 추출
-var arr = [1, 2, 3];
-var arr2 = ['4', '5'];
-var arr3 = __spreadArray(__spreadArray([], arr, true), arr2, true);
-console.log(arr3);
-// destructuring 활용해서 배열과 오브젝트 안에있는 항목을 바로 변수에 할당
-var arr4 = [1, 2];
-var a = arr4[0], b = arr4[1];
-console.log(a);
-console.log(b);
-var _a = { student: true, age: 20 }, student = _a.student, age = _a.age;
-console.log(student);
-console.log(age);
-var obj = { student: true, age: 20 };
-var func2 = function (_a) {
-    var student = _a.student, age = _a.age;
-    console.log(student, age);
-};
-func2(obj);
-// (숙제1) 숫자 여러개를 입력하면 최댓값을 return 해주는 함수를 만들어봅시다.
-// 최댓값(6,3,7,2) 이렇게 쓰면 7이 return 되어야합니다.
-// (조건1) 넣을 수 있는 숫자 갯수는 제한없음, 0 이상의 정수만 가능합니다.
-// (조건2) Math.max() 사용금지 반복문이나 쓰셈
-var homework = function () {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
+func('a');
+var func2 = function (animal) {
+    if ('swim' in animal) {
+        console.log('물고기!');
     }
-    return args.reduce(function (a, b) {
-        if (a > b) {
-            return a;
-        }
-        else {
-            return b;
-        }
-    });
+    else {
+        console.log('새!');
+    }
 };
-console.log(homework(6, 30, 7, 2));
-var homework2 = function (_a) {
-    var user = _a.user, comment = _a.comment, admin = _a.admin;
-    console.log(user);
-    console.log(comment);
-    console.log(admin);
+func2({ swim: 'aaa' });
+var func3 = function (x) {
+    if (x.wheel === '4개') {
+        console.log('차!');
+    }
+    else {
+        console.log('오토바이!');
+    }
 };
-homework2({ user: 'kim', comment: [3, 5, 4], admin: false });
-// (숙제3) 이렇게 생긴 array 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
-// 함수( [40, 'wine', false] )
-// 어떻게 코드를 짜야할까요?
-// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
-// (조건2) 함수실행시 입력한 파라미터들을 전부 콘솔창에 출력해줘야합니다.
-var homework3 = function (_a) {
-    var a = _a[0], b = _a[1], c = _a[2];
-    console.log(a, b, c);
-};
-homework3([40, 'wine', false]);
+func3({ wheel: '4개', color: 'blue' });
--- index.ts
+++ index.ts
@@ -1,68 +1,46 @@
-// rest parameter 파라미터가 몇개일지 모를 때 사용
-const func = (...args: number[]) => {
-    console.log(args)
-}
-func(1,2,3,4,56,67,65,54,4,23,2);
+// null & undefined 체크 팁
+const func = (a: string | undefined): void => {
+    if(a && typeof a === 'string') {
+        // a가 null 또는 undefined 일때는 if문 실행이 안됨 false 로 인식함 바로
+        // 익숙하지 않으면 안쓰는게 나음 ㅇㅇ
+    } else {
 
-// spread operator 배열, 오브젝트의 내용물만 추출
-const arr: number[] = [1, 2, 3];
-const arr2: string[] = ['4', '5'];
-const arr3: (number | string)[] = [...arr, ...arr2];
-console.log(arr3);
-
-// destructuring 활용해서 배열과 오브젝트 안에있는 항목을 바로 변수에 할당
-const arr4: number[] = [1, 2];
-const [a, b] = arr4;
-console.log(a);
-console.log(b);
-const {student, age} = {student: true, age: 20}
-console.log(student);
-console.log(age);
-
-const obj = {student: true, age: 20}
-const func2 = ({student, age}: {student: boolean, age: number}) => {
-    console.log(student, age);
+    }
 }
-func2(obj);
+func('a');
 
-// (숙제1) 숫자 여러개를 입력하면 최댓값을 return 해주는 함수를 만들어봅시다.
-// 최댓값(6,3,7,2) 이렇게 쓰면 7이 return 되어야합니다.
-// (조건1) 넣을 수 있는 숫자 갯수는 제한없음, 0 이상의 정수만 가능합니다.
-// (조건2) Math.max() 사용금지 반복문이나 쓰셈
-const homework = (...args: number[]): number => {
-    return args.reduce((a: number, b: number): number => {
-        if(a > b) {
-            return a;
-        } else {
-            return b;
-        }
-    });
+// 내로잉 할 수 있는 방법들
+// typeof를 사용할 수 없는 케이스에는 in을 사용하자
+interface Fish {
+    swim: string
 }
-console.log(homework(6, 30, 7, 2));
+interface Bird {
+    fly: string
+}
+const func2 = (animal: Fish | Bird): void => {
+    if('swim' in animal) {
+        console.log('물고기!');
+    } else {
+        console.log('새!');
+    }
+}
+func2({swim: 'aaa'});
 
-// (숙제2) 이렇게 생긴 object 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
-// 함수( { user : 'kim', comment : [3,5,4], admin : false } )
-// 어떻게 코드를 짜야할까요?
-// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
-// (조건2) 함수실행시 입력한 파라미터의 value들 (kim, [3,5,4] 이런거)을 전부 콘솔창에 출력해줘야합니다.
-interface Homework2 {
-    user: string,
-    comment: number[],
-    admin: boolean
+// in은 고유한 속성에만 활용할 수 있음 잇몸으로 떼우자(원래는 한가지 타입으로 만들겠지.. wheel과 color니까\
+// 리터럴타입으로 고유한 값을 넣어주자
+interface Car {
+    wheel: '4개',
+    color: string
 }
-const homework2 = ({user, comment, admin}: Homework2): void => {
-    console.log(user);
-    console.log(comment);
-    console.log(admin);
+interface Bike {
+    wheel: '2개',
+    color: string
 }
-homework2({user : 'kim', comment : [3,5,4], admin : false});
-
-// (숙제3) 이렇게 생긴 array 자료를 파라미터로 입력할 수 있는 함수를 만들어봅시다.
-// 함수( [40, 'wine', false] )
-// 어떻게 코드를 짜야할까요?
-// (조건1) 오늘 배운 파라미터 destructuring 문법을 써봅시다.
-// (조건2) 함수실행시 입력한 파라미터들을 전부 콘솔창에 출력해줘야합니다.
-const homework3 = ([a, b, c]: (string | number | boolean)[]): void => {
-    console.log(a, b, c);
+const func3 = (x: Car | Bike) => {
+    if(x.wheel === '4개') {
+        console.log('차!');
+    } else {
+        console.log('오토바이!');
+    }
 }
-homework3([40, 'wine', false]);
+func3({wheel: '4개', color: 'blue'});
